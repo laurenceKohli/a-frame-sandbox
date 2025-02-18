49023216400a8965c61cc530515f230407f99227
laurenceKohli <laurence.kohli@hes-so.com>
2025-02-18 08:50:09 +0100

    minimum scene

--- src/Aframe.vue
+++ src/Aframe.vue
@@ -13,11 +13,9 @@
 
   <!-- The DOM element of the overlay must be mounted before the A-Frame Scene is mounted -->
   <!-- Otherwise the "webxr system" of the A-Frame scene wont find the DOM Element -->
-  <TheOverlay v-model="scale" id="overlay" />
+  <!-- <TheOverlay v-model="scale" id="overlay" /> -->
 
   <TheScene
-    :scale="scale"
-    overlay-selector="#overlay"
     @loaded="loaded = true"
   />
 </template>
