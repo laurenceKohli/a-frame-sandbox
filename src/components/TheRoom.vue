8b336234b551c66930e30999b93b6af2863914db
laurenceKohli <laurence.kohli@hes-so.com>
2025-02-24 11:39:06 +0100

    initial setup

---
+++ src/components/TheRoom.vue
@@ -0,0 +1,29 @@
+<script setup>
+  import PortalTeleporter from './PortalTeleporter.vue';
+
+</script>
+
+<template>
+  <a-entity
+    id="main-room"
+    gltf-model="#room"
+    rotation="0 90 0"
+    position="0 0 -5"
+    scale="1 1.1 1"
+  >
+    <PortalTeleporter
+      label="Enter the Life Cube Room"
+      material="src: #room-physic-texture"
+      position="-7.99 1.5 0"
+      rotation="0 90 0"
+      :rot="0"
+      :y="100"
+      :z="1"
+      :cameraEffect="true"
+      :cameraY="101.65"
+      :cameraZ="-2"
+      :cameraRot="-180"
+    />
+  </a-entity>
+
+</template>
--- src/components/TheScene.vue
+++ src/components/TheScene.vue
@@ -2,17 +2,7 @@
   import { ref } from 'vue';
 
   import TheCameraRig from './TheCameraRig.vue';
-  import TheMainRoom from './TheMainRoom.vue';
-  import TheLifeCubeRoom from './TheLifeCubeRoom.vue';
-  import ThePhysicRoom from './ThePhysicRoom.vue';
-
-  import '../aframe/simple-grab.js';
-  import '../aframe/outline.js';
-
-  defineProps({
-    scale: Number,
-    overlaySelector: String,
-  });
+  import TheRoom from './TheRoom.vue';
 
   const allAssetsLoaded = ref(false);
 </script>
@@ -20,48 +10,17 @@
 <template>
   <a-scene
     background="color: black;"
-    :webxr="`
-      requiredFeatures: local-floor;
-      referenceSpaceType: local-floor;
-      optionalFeatures: dom-overlay;
-      overlayElement: ${overlaySelector};
-    `"
-    xr-mode-ui="XRMode: xr"
-    physx="
-      autoLoad: true;
-      delay: 1000;
-      useDefaultScene: false;
-      wasmUrl: lib/physx.release.wasm;
-    "
-    outline
-    simple-grab
   >
 
     <a-assets @loaded="allAssetsLoaded = true">
-      <!--
-        Title: VR Gallery
-        Model source: https://sketchfab.com/3d-models/vr-gallery-1ac32ed62fdf424498acc146fad31f7e
-        Model author: https://sketchfab.com/mvrc.art (Maxim Mavrichev)
-        Model license: CC BY 4.0 ( https://creativecommons.org/licenses/by/4.0/ )
-      -->
-      <a-asset-item id="room" src="assets/vr_gallery.glb"></a-asset-item>
-      <!--
-        Title: 3D Gallery for VR projects
-        Model source: https://sketchfab.com/3d-models/3d-gallery-for-vr-projects-68f77ed8558c4bd59e0a13e2cc9d1fd1
-        Model author: https://sketchfab.com/tekuto1s (tekuto1s)
-        Model license: CC BY 4.0 ( https://creativecommons.org/licenses/by/4.0/ )
-      -->
-      <a-asset-item id="physic-room" src="assets/3d_gallery_for_vr_projects.glb"></a-asset-item>
-      <a-asset-item id="sound-1" response-type="arraybuffer" src="assets/sound1.mp3" preload="auto"></a-asset-item>
-      <img id="room-physic-out-texture" :src="`assets/main-room-from-physic-room.png`">
-      <img id="room-gol-out-texture" :src="`assets/main-room-from-gol-room.png`">
-      <img id="room-physic-texture" :src="`assets/physicRoom.png`">
+      <a-asset-item id="house-obj" src="./assets/models/isometric_low_poly_house.glb"></a-asset-item>
     </a-assets>
 
+    <a-gltf-model position="0 -0.85 -7.9" scale="1.05 1.05 1.05" src="#house-obj"></a-gltf-model>
+
     <template v-if="allAssetsLoaded">
-      <TheMainRoom :scale="scale" />
-      <TheLifeCubeRoom />
-      <ThePhysicRoom />
+      <a-box color="red"></a-box>
+      <TheRoom></TheRoom>
     </template>
 
     <TheCameraRig />
